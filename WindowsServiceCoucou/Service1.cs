762b132a32e15139fcac9cdeb65e0ffa717f58d8
D-Peltre <dylan.peltre@gmail.com>
2017-02-27 15:17:33 +0100

    Rajout du code

---
+++ WindowsServiceCoucou/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceCoucou
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            Timer t = new Timer();
+            t.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            t.Interval = 60000;
+            t.Start();
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            String Source = "TestService";
+            String Log = "Application";
+            String Event = "Coucou";
+
+            if (!EventLog.SourceExists(Source))
+            {
+                EventLog.CreateEventSource(Source, Log);
+            }
+
+            EventLog.WriteEntry(Source, Event);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
